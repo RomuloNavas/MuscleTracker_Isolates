c9ab977e469f6f6645114884fd69c31d032a1060
RomuloNavas <navas99romulo@gmail.com>
2023-02-03 22:29:55 -0500

    removes unnecessary vars. Updates font

--- lib/theme.dart
+++ lib/theme.dart
@@ -12,132 +12,107 @@ class AppTheme {
         // primary: const Color(0xff5181b8),
         secondary:
             const Color(0xff0058e4), // Color got the overflow glow effect
-        tertiary: const Color(0xffe2e3e5),
+
+        /// Used just for icons
+        tertiary: const Color(0xff22272f),
         surface: const Color.fromRGBO(232, 233, 234, 1), // card color
+        surfaceVariant: const Color(0xfff2f3f5),
         error: const Color(0xffe40031),
-        outline: const Color(0xffd3d5d7),
+        outline: const Color(0xffe7e8ec),
         shadow:
             const Color(0xff838997) // Used for icons in buttonIcons and labels
         ),
     scrollbarTheme: ScrollbarThemeData().copyWith(
-      thumbColor: MaterialStateProperty.all(const Color(0xff1f5cff)),
+      // thumbColor: MaterialStateProperty.all(appTheme.primaryColor),
       trackColor: MaterialStateProperty.all(const Color(0xff212224)),
     ),
     brightness: Brightness.light,
-    dividerColor: Color(0xffd5d6d8),
+    dividerColor:
+        Colors.transparent, // Color by default of separation lines in tables
     useMaterial3: false,
     textTheme: appTextTheme,
     scaffoldBackgroundColor: const Color(0xffffffff),
-    cardColor: const Color(0xfff2f3f5), // ✅
-    primaryColor: const Color(0xff5181b8), // ✅
     hoverColor: const Color.fromARGB(255, 0, 0,
         0), // `InkWell` → Background color on tap. && `Button` → Background color on tap.
     buttonColor: const Color(0xffd3d5d7),
     // In the Inkwell Widget, splashColor is the color the appears like in waves (it gives the sensation of tap),
-    //highlightColor appears when you long press the InkWell widget.
-    // highlightColor is also defines the color for the scrollbar thumb.
+    //colorScheme.surfaceVariant appears when you long press the InkWell widget.
+    // colorScheme.surfaceVariant is also defines the color for the scrollbar thumb.
     splashColor: const Color(0xffbfbfbf),
-    highlightColor: const Color(0xffd3d5d7),
     accentColor: const Color(0xfff0f2f5), // Color got the overflow glow effect
     unselectedWidgetColor: const Color(0xffd9d9d9),
     focusColor: const Color(0xff148dc6),
     hintColor: const Color(0xff45cf69),
-    errorColor: const Color(0xffC42C17), // ✅
-
-    appBarTheme: AppBarTheme(
-      backgroundColor: const Color(0xfff2f3f5),
-      elevation: 0,
-      toolbarHeight: 80,
-      titleTextStyle: GoogleFonts.roboto(
-          fontSize: 22,
-          fontWeight: FontWeight.w400,
-          letterSpacing: 0.4,
-          color: Colors.black),
-    ),
   );
 
   static ThemeData appDarkTheme = ThemeData.dark().copyWith(
     colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: const Color(0xff071eff),
-        secondary:
-            const Color(0xff0058e4), // Color got the overflow glow effect
-        tertiary: const Color(0xff282828),
-        surface: const Color(0xff212224),
-        error: const Color(0xffe40031),
-        outline: const Color(0xff333333),
-        shadow: const Color(0xff838997)),
+      primary: const Color(0xff071eff),
+      secondary: const Color(0xff0058e4),
+      tertiary: const Color(0xffe8eaed),
+      // ⬇ Used for cards (important sections containers)
+      surface: const Color(0xff232529),
+      surfaceVariant: const Color(0xff181818), // Used in text input background
+      outline: const Color(0xff333333),
+      error: const Color(0xffe40031),
+      shadow: const Color(0xff838997),
+    ),
+
     scrollbarTheme: ScrollbarThemeData().copyWith(
-      thumbColor: MaterialStateProperty.all(const Color(0xff54ff81)),
+      // thumbColor: MaterialStateProperty.all(appDarkTheme.primaryColor),
       trackColor: MaterialStateProperty.all(const Color(0xff212224)),
     ),
     brightness: Brightness.dark,
     useMaterial3: false,
     textTheme: appTextThemeDark,
-    dividerColor: const Color(0xff333333), //✅
-    // dividerColor: const Color(0xff202c45), //✅
-    scaffoldBackgroundColor: const Color(0xff0f0f0f),
-    cardColor: const Color(0xff212224),
-    primaryColor: Color(0xff45cf69),
-    // const Color(0xff4a98f7),
+    dividerColor: Colors.transparent,
+    scaffoldBackgroundColor: const Color(0xff121212),
     hoverColor: const Color.fromARGB(255, 0, 0,
         0), // `InkWell` → Background color on tap. && `Button` → Background color on tap.
     buttonColor: const Color(0xff1a1a1a),
 
     // In the Inkwell Widget, splashColor is the color the appears like in waves (it gives the sensation of tap),
-    //highlightColor appears when you long press the InkWell widget.
-    // highlightColor is also defines the color for the scrollbar thumb.
+    //colorScheme.surfaceVariant appears when you long press the InkWell widget.
+    // colorScheme.surfaceVariant is also defines the color for the scrollbar thumb.
     splashColor: const Color(0xffdcdcdc),
-    highlightColor: const Color(0xff333333),
     accentColor:
         const Color(0xfff0f2f5), //✅ // Color got the overflow glow effect
     unselectedWidgetColor: const Color(0xffd9d9d9),
     focusColor: const Color(0xff148dc6),
     hintColor: const Color(0xff45cf69),
-    errorColor: const Color(0xffC42C17), //✅
-
-    appBarTheme: AppBarTheme(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        toolbarHeight: 80,
-        titleTextStyle: GoogleFonts.roboto(
-          fontSize: 22,
-          fontWeight: FontWeight.w400,
-          letterSpacing: 0.4,
-          color: Get.isDarkMode ? Colors.white : Colors.black,
-        )),
   );
 
   static TextTheme appTextTheme = TextTheme(
     headline1: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 26,
         color: Color(0xff242a2f),
         letterSpacing: 0.3,
         height: 0.9,
         fontWeight: FontWeight.w600),
     headline2: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 24,
         color: Color(0xff242a2f),
         fontWeight: FontWeight.w600),
     headline3: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 22,
         color: Color(0xff242a2f),
         fontWeight: FontWeight.w500),
     headline4: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 20,
         color: Color(0xff242a2f),
         fontWeight: FontWeight.w500),
     headline5: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 18,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.4,
         color: Colors.black),
     headline6: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 16,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.4,
@@ -160,7 +135,7 @@ class AppTheme {
       fontSize: 14,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.4,
-      color: Color(0xff242a2f),
+      color: Color(0xffadadad),
     ),
     overline: GoogleFonts.roboto(
         fontSize: 14,
@@ -171,35 +146,35 @@ class AppTheme {
 
   static TextTheme appTextThemeDark = TextTheme(
     headline1: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 26,
         color: Color(0xffeeeeee),
         letterSpacing: 0.3,
         height: 0.9,
         fontWeight: FontWeight.w600),
     headline2: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 24,
         color: Color(0xffeeeeee),
         fontWeight: FontWeight.w600),
     headline3: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 22,
         color: Color(0xffcecece),
         fontWeight: FontWeight.w500),
     headline4: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 20,
         color: Color(0xffcecece),
         fontWeight: FontWeight.w500),
     headline5: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 18,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.4,
         color: Color(0xffcecece)),
     headline6: TextStyle(
-        fontFamily: 'inputmono',
+        fontFamily: 'oceanwide',
         fontSize: 16,
         fontWeight: FontWeight.w500,
         letterSpacing: 0.4,
@@ -222,7 +197,7 @@ class AppTheme {
       fontSize: 15,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.4,
-      color: const Color(0xffeeeeee),
+      color: const Color(0xffadadad),
     ),
     overline: GoogleFonts.roboto(
       fontSize: 14,
